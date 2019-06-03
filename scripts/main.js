6d5dc90e6529dcdbe21a7712fe6467b3afd90ea5
njuno <pnmjuno@yahoo.com>
2019-06-03 18:44:35 +0300

    fixes

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,13 @@ $(function() {
 
      var navMain = $("#navbarContent");
 
+     var nb = $(".navbar-brand");
+
      navMain.on("click", "a", null, function () {
+         navMain.collapse('hide');
+     });
+
+     nb.on("click", function () {
          navMain.collapse('hide');
      });
 
