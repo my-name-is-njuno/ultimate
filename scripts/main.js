7f7c8af2580423bf2dfa8f2dd1623843f2f6baab
njuno <pnmjuno@yahoo.com>
2019-07-01 17:43:22 +0300

    mm

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,88 @@ $(function() {
      });
 
  });
+
+
+
+
+
+
+
+
+
+
+
+
+jQuery(document).ready(function(){
+            jQuery('#bappt').click(function(e){
+               e.preventDefault();
+               
+
+               		 var name = $("#name").val();
+			  		if (name == "") {
+				        alert("Name not Filled")
+				        return false;
+				      }
+			  	  var email = $("#email").val();
+			  		if (email == "") {
+				        alert("Email not Filled")
+				        return false;
+				      }
+			  	  var phone = $("#phone").val();
+			  		if (phone == "") {
+				       alert("Phone number not Filled")
+				        return false;
+				     }
+
+				   var proposed_date = $("#proposed_date").val();
+			  		if (proposed_date == "") {
+				        alert("Proposed Date not Filled")
+				        return false;
+				      }
+
+				   var subject = $("#subject").val();
+			  		if (subject == "") {
+				        alert("subject not Filled")
+				        return false;
+				      }
+
+				   var message = $("#message").val();
+			  		if (message == "") {
+				        alert("message not Filled")
+				        return false;
+				      }
+
+
+
+               jQuery.ajax({
+
+                type: 'POST',
+                url: "sendemail.php",
+                data: {
+                     name: jQuery('#name').val(),
+                     email: jQuery('#email').val(),
+                     subject: jQuery('#subject').val(),
+                     phone: jQuery('#phone').val(),
+                     proposed_date: jQuery('#proposed_date').val(),
+                     message: jQuery('#message').val(),
+                    
+                  },
+                  success: function(result){
+                    
+
+                    jQuery('#name').val("") 
+                    jQuery('#email').val("")
+                    jQuery('#subject').val("")
+                    jQuery('#message').val("")
+                    jQuery('#phone').val("")
+                    jQuery('#proposed_date').val("")
+                    
+                     alert(result);
+            }});
+            });
+
+
+
+            
+               
+    });
